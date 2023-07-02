57e5ac2ad97980a5e43fa3a6fa7409dddfe267ef
Tobias Grothmann <tobiasgrothmannplotterart@gmail.com>
2023-07-02 20:16:25 +0200

    add ws812 controller test

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,29 @@ fn main() {
 
     stream.play().expect("couldn ot start stream");
     std::thread::sleep(std::time::Duration::from_secs(5));
+
+    let mut controller = ControllerBuilder::new()
+        .freq(800_000)
+        .dma(10)
+        .channel(
+            0, // Channel Index
+            ChannelBuilder::new()
+                .pin(10) // GPIO 10 = SPI0 MOSI
+                .count(64) // Number of LEDs
+                .strip_type(StripType::Ws2812)
+                .brightness(20) // default: 255
+                .build(),
+        )
+        .build()
+        .unwrap();
+
+    let leds = controller.leds_mut(0);
+
+    for led in leds {
+        *led = [0, 0, 255, 0];
+    }
+
+    controller.render().unwrap();
 }
 
 fn audio_in_callback<T, U>(signal_arr: &[f32], _sample_rate: f64, rms_history: &mut ValueHistory) {
